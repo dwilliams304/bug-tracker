4d0f2acb9eaa2228576411644cd3da8b9951a195
dwilliams304 <d.williams0434@gmail.com>
2023-12-08 17:11:00 -0700

    Frontend restructuring

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/main/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
