c7e8c05c6f6a1790a25ad561e47e31ae89a652c2
dwilliams304 <d.williams0434@gmail.com>
2023-12-09 19:11:16 -0700

    Ticket list styling

--- frontend/src/components/common/Ticket.jsx
+++ frontend/src/components/common/Ticket.jsx
@@ -1,9 +1,11 @@
 export default function Ticket(){
     return(
-        <tr className="ticket" style={{backgroundColor: "cyan"}}>
+        <tr className="ticket">
+            <td>Project 1</td>
             <td>0afe34-d592et</td>
             <td>Data loss in account page</td>
             <td>10/28/2023</td>
+            <td style={{textAlign: "center"}}><span className="severity">Critical</span></td>
             <td>Robert, Sarah</td>
             <td>41d</td>
             <td>UI, Data, Security</td>
--- frontend/src/components/layout/TicketList.jsx
+++ frontend/src/components/layout/TicketList.jsx
@@ -1,22 +1,29 @@
-import Ticket from "../common/Ticket"
-
+import Ticket from "../common/Ticket";
+import '../../styles/page-styles/tickets.css';
 
 export default function TicketList(){
     return(
-        <table className="tickets-table">
-            <thead className="tickets-header">
-                <td>ticket-id</td>
-                <td>title</td>
-                <td>creation-date</td>
-                <td>severity</td>
-                <td>assigned-to</td>
-                <td>days-open</td>
-                <td>Tags</td>
-            </thead>
+        <div className="table-container">
+            <table className="tickets-table">
+                <thead className="tickets-header">
+                    <tr>
+                        <th>project</th>
+                        <th>ticket-id</th>
+                        <th>title</th>
+                        <th>creation-date</th>
+                        <th style={{textAlign: "center"}}>severity</th>
+                        <th>assigned-to</th>
+                        <th>days-open</th>
+                        <th>Tags</th>
+                    </tr>
+                </thead>
 
-            <tbody className="tickets-list">
-                <Ticket />
-            </tbody>
-        </table>
+                <tbody className="tickets-list">
+                    <Ticket />
+                    <Ticket />
+                    <Ticket />
+                </tbody>
+            </table>
+        </div>
     )
 }
