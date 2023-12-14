1763d07c09db5f679a88c5539eb17d8965d1da05
dwilliams304 <d.williams0434@gmail.com>
2023-12-14 11:35:23 -0700

    Remove initial roles/users

--- backend/data/initial-data/roles.js
+++ backend/data/initial-data/roles.js
@@ -1,14 +1,5 @@
 const roles = [
-    { role_name: 'Head of Engineering' },
-    { role_name: 'FE Manager' },
-    { role_name: 'BE Manager' },
-    { role_name: 'Security Manager' },
-    { role_name: 'FE Engineer' },
-    { role_name: 'BE Engineer' },
-    { role_name: 'Data Analyst' },
-    { role_name: 'Security Engineer' },
-    { role_name: 'Support Engineer' },
-    { role_name: 'Staff Engineer' },
+    { role_name: 'admin' },
 ]
 
 module.exports = roles;
--- backend/data/initial-data/users.js
+++ backend/data/initial-data/users.js
@@ -1,12 +1,5 @@
 const users = [
-    { user_name: 'Robert', password: 'pw', role_id: 0},
-    { user_name: 'Jason', password: '', role_id: 7},
-    { user_name: 'Sarah', password: '', role_id: 3},
-    { user_name: 'Jack', password: '', role_id: 9},
-    { user_name: 'Johnston', password: '', role_id: 9},
-    { user_name: 'Malik', password: '', role_id: 7},
-    { user_name: 'Michael', password: '', role_id: 9},
-    { user_name: 'Lauren', password: '', role_id: 9},
+    { user_name: 'admin', password: 'password', role_id: 0},
     
 ]
 
