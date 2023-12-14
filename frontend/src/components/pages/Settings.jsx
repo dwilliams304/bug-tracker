01b965b6f8c8d8e0d951a2e531e8666bc643a4f7
dwilliams304 <d.williams0434@gmail.com>
2023-12-14 10:55:49 -0700

    Settings layout, add MUI

---
+++ frontend/src/components/pages/Settings.jsx
@@ -0,0 +1,35 @@
+import '../../styles/page-styles/settings.css';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import SettingsIcon from '@mui/icons-material/Settings';
+import InfoIcon from '@mui/icons-material/Info';
+
+const Settings = () => {
+    return(
+        <div className="settings">
+            <aside className="sidebar">
+                <a href='#' className='active'> <EmojiPeopleIcon />
+                    <h3>Roles</h3>
+                </a>
+                <a href='#'><ContactsIcon />
+                    <h3>Users</h3>
+                </a>
+                <a href='#'><ReceiptLongIcon />
+                    <h3>Tickets</h3>
+                </a>
+                <a href='#'><SettingsIcon />
+                    <h3>General</h3>
+                </a>
+                <a href='#'><InfoIcon />
+                    <h3>Info</h3>
+                </a>
+            </aside>
+            <div className="menu">
+
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/main/index.css';
 import './styles/main/main.css';
 import NavBar from './components/layout/NavBar';
+import Settings from './components/pages/Settings';
 import BugTracker from './components/pages/BugTracker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -17,7 +18,8 @@ function App() {
   return (
     <>
       <NavBar />
-      <BugTracker />
+      <Settings />
+      {/* <BugTracker /> */}
     </>
   );
 }
